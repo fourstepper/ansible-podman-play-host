85d0d1141315fb075210b79517451b26cc9c4012
Robin Opletal <me@robinopletal.com>
2023-11-17 09:13:48 +0100

    feat(playbook): various

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i inventory -u root playbook.yml --vault-password-file=~/.ansible/personal_secret.txt
+ansible-playbook -i inventory -u root playbook.yml --vault-password-file=~/.ansible/personal_secret.txt --diff
